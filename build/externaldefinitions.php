315c0795394159cd7fae30c4ba1e0306dccbb47c
Matias Griese <matias@kunena.org>
2011-10-20 15:12:59 +0300

    Fix problems reported by Zend Studio: missing DOCTYPE in XML files

---
+++ build/externaldefinitions.php
@@ -0,0 +1,77 @@
+<?php
+// Never use this file in any code - it is only here to provide declarations for external functions
+die();
+
+// dummy definitions of external classes and functions to avoid zend studio warnings and errors
+
+// JomSocial
+class CActivityStream {}
+class CBookmarks {}
+class CApplications {}
+class CAssets {}
+class CAppPlugins {}
+class CCategoryHelper {}
+class CEditor {}
+class CError {}
+class CEventHelper {}
+class CFactory {}
+class CFeatured {}
+class CFilterBar {}
+class CFormElement {}
+class CGroupHelper {}
+class CInvitation {}
+class CLike {}
+class COwnerHelper {}
+class CParameter {}
+class CPluginHelper {}
+class CReportingLibrary {}
+class CRoute {}
+class CStringHelper {}
+class CTemplate {}
+class CTimeHelper {}
+class CUrlHelper {}
+class CVideoLibrary {}
+class CWallLibrary {}
+class CWindow {}
+class CommunityView {}
+function cAvatarTooltip() {}
+function cIsPlural() {}
+
+// Finder
+class FinderIndexerAdapter {}
+class FinderIndexer {}
+class FinderIndexerHelper {}
+class FinderIndexerQueue {}
+
+// Nooku
+class Koowa {}
+
+// mtree
+class mAdvancedSearch {}
+class mFields {}
+class mtCats {}
+class mtCL_main0 {}
+class mtImage {}
+class mtImages {}
+class mtLinks {}
+class mtListListing {}
+class mtLog {}
+class mtPathWay {}
+function mtAppendPathWay() {}
+function rss() {}
+
+// savant
+
+class Savant2 {}
+function assignCommonListlinksVar() {}
+function assignCommonVar() {}
+function assignCommonViewlinkVar() {}
+function loadCustomTemplate() {}
+function loadFields() {}
+function loadLink() {}
+
+// phpThumb
+class phpThumb {}
+
+// Pear: HTML select
+class HTML_Select {}
